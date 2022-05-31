aa92659f9ad486e2bc7483c1ea96daa783343d45
Alan Doyle <alanduvgall@gmail.com>
2022-05-31 04:50:05 +0100

    add four basic calculator operations as functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {                        // add two numbers. For each function below, will need to use DOM methods to take input from calculator and amend a and b while storing value
+    
+    return a + b;
+}
+
+function subtract(a, b) {
+    
+    return a - b;
+}
+
+function multiply(a, b) {
+
+    return a * b;
+}
+
+function divide(a, b) {
+
+    return a / b;
+}
+
+console.log(add(4, 5));                     // testing basic functionality
+console.log(subtract(13, 5));
+console.log(multiply(2, 4));
+console.log(divide(24, 3));
