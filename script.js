e95add2d79c3a2055888d5482ec6149887a265e1
Alan Doyle <alanduvgall@gmail.com>
2022-05-31 05:08:20 +0100

    add operator function and html buttons and textarea for display

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ console.log(add(4, 5));                     // testing basic functionality
 console.log(subtract(13, 5));
 console.log(multiply(2, 4));
 console.log(divide(24, 3));
+
+function operator(a, b, symbol) {           // function to call one of the four basic function depending on user input
+    if(symbol == '+') {
+        return add(a, b);
+    }else if(symbol == '-') {
+        return subtract(a, b);
+    } else if(symbol == '*') {
+       return multiply(a, b);
+    } else if(symbol == '/') {
+        return divide(a, b);
+    }
+
+}
+console.log(operator(4, 4, '+'));
+console.log(operator(4, 4, '-'));
+console.log(operator(4, 4, '*'));
+console.log(operator(4, 4, '/'));
